34d3b67c1ec24c1e85b8e7442b6ceb120dd00835
Iacopo <idonda@users.noreply.github.com>
2020-02-03 13:38:25 +0100

    Create install_composer.sh

---
+++ install_composer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
